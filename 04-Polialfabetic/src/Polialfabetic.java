af12efcb7553033c86525bd69bf6ff9e0f6d4cf7
Mikleiper <mimuba55@gmail.com>
2025-10-15 21:20:36 +0200

    acabat

--- 04-Polialfabetic/src/Polialfabetic.java
+++ 04-Polialfabetic/src/Polialfabetic.java
@@ -3,20 +3,15 @@ import java.util.*;
 public class Polialfabetic {
     
     private static final char[] ALFABETMAJUS = {'A','À','Á','B','C','Ç','D','E','È','É','F','G','H','I','Ì','Í','Ï','J','K','L','M','N','Ñ','O','Ò','Ó','P','Q','R','S','T','U','Ù','Ú','Ü','V','W','X','Y','Z'};
-    private static char[] permutat;
+    private static char[] permutatMajus;
     private static Random random;
     private static final long CLAU = 1234;
 
-    private static int index(char c, boolean majuscula, char [] abc) { //en comptes de convertir a String busquem directament al array
+    //funció index més neta, nomes comprova int index, no majuscules/minuscules
+    private static int index(char c, char [] abc) {
         for (int i = 0; i < abc.length; i++){
-            if (majuscula){
-                if(abc[i] == c){
-                    return i;
-                }
-            } else {
-                if (Character.toLowerCase(abc[i]) == c){
-                    return i;
-                }
+            if (abc[i] == Character.toUpperCase(c)){
+                return i;
             }
         }
         return -1;
@@ -31,52 +26,65 @@ public class Polialfabetic {
         List<Character> llistaChar = new ArrayList<>();
         for(char c : ALFABETMAJUS){
             llistaChar.add(c);
-        }
-        
-        //permutem
-        initRandom(CLAU);
+        }        
+        //permutem amb llavor random executada abans per void initRandom
         Collections.shuffle(llistaChar, random);
-
         //tornem a convertir a char array
-        permutat = new char [llistaChar.size()];
-        for (int i = 0; i < permutat.length; i ++){
-            permutat[i] = llistaChar.get(i);
+        permutatMajus = new char [llistaChar.size()];
+        for (int i = 0; i < permutatMajus.length; i ++){
+            permutatMajus[i] = llistaChar.get(i);
         }
     }
 
-    public static String xifraPoliAlfa(String msg){
+    private static String xifraPoliAlfa(String msg){
+        
         StringBuilder solucio = new StringBuilder();
-        permutaAlfabet();
-
-        for (char c :msg.toCharArray()){
-            boolean majuscula = Character.isUpperCase(c); // mirar si és minúscula o majuscula
-            int idx = index(c,majuscula, ALFABETMAJUS); 
-            if (idx != -1) { // si es compleix l'if significa que és un char en majuscula
-                
-                char trobat = 
+        for (char c : msg.toCharArray()) {
+            permutaAlfabet(); //permutem cada caracter
+            int idx = index(c, ALFABETMAJUS); 
+            if (idx != -1) {
+                char trobat = permutatMajus[idx]; 
+                solucio.append(Character.isUpperCase(c) ? trobat : Character.toLowerCase(trobat)); // si és majuscula True append directament, si és minuscula False append tolowercase
+            } else {
+                solucio.append(c); // no és un char de l'alfabet
             }
         }
-        
-        
-        for (int i = 0; i < msg.length(); i ++){
-            char c = msg.charAt(i);
-            if c isCharacter
+        return solucio.toString();
+    }
+    
+    public static String desxifraPoliAlfa(String msgXifrat){
+        StringBuilder solucio = new StringBuilder();
+        for (char c : msgXifrat.toCharArray()) {
+            permutaAlfabet(); //permutem cada caracter   EL PROBLEMA ES Q PERMUTA TB LOS ESPACIOS!!! HAY Q PONER UN IF Q FILTRE PRIMERO LOS ESPACIOS!!!!!!
+            int idx = index(c, permutatMajus);
+            if (idx != -1) {
+                char trobat = ALFABETMAJUS[idx]; 
+                solucio.append(Character.isUpperCase(c) ? trobat : Character.toLowerCase(trobat)); // si és majuscula True append directament, si és minuscula False append tolowercase
+            } else {
+                solucio.append(c); // no és un char de l'alfabet
+            }
+        }
+        return solucio.toString();
+    }
 
-            permutaAlfabet();
-            c = permutat[i];
+    public static void main(String[] args) {
+        String msgs[] = {"Test 01 Àrbitre, coixí, Perímetre",
+                "Test 02 Taüll, DÍA, año",
+                "Test 03 Peça, Òrrius, Bóvila"};
+        String msgsXifrats[] = new String[msgs.length];
 
+        System.out.println("Xifratge:\n--------");
+        for (int i = 0; i < msgs.length; i++) {
+            initRandom(CLAU);
+            msgsXifrats[i] = xifraPoliAlfa(msgs[i]);
+            System.out.printf("%-34s -> %s%n", msgs[i], msgsXifrats[i]);
+        }
 
-
+        System.out.println("Desxifratge:\n---------");
+        for (int i = 0; i < msgs.length; i++) {
+            initRandom(CLAU);
+            String msg = desxifraPoliAlfa(msgsXifrats[i]);
+            System.out.printf("%-34s -> %s%n", msgsXifrats[i], msg);
         }
     }
-
-    public static String desxifraPoliAlfa(String msgXifrat){
-
-    }
-
-
-
 }
-
-
-// Collections.shuffle(permutat, random);
